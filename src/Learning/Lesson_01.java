5c9cdb603bdcaf1b73beb57bc476c7dfc8789306
Alexey <vaznoe2024@mail.ru>
2025-10-04 23:45:52 +0300

    v.0 Создание проекта и первого класса файла

---
+++ src/Learning/Lesson_01.java
@@ -0,0 +1,28 @@
+package Learning;
+public class Lesson_01 {
+    public static void main(String[] args){
+        System.out.println("Hello, world!");
+
+        short age = 25;
+        short girlfriends_age = 23;
+        System.out.print("Ваш общий возраст " + (age + girlfriends_age));
+        System.out.println("!");
+
+        int counter = 0;
+        counter += 5;
+        counter ++;
+        counter --;
+        System.out.println("Счетчик " + counter);
+
+        double double_counter = counter;
+        int BackToInt_counter = (int) counter;
+    }
+}
+/*
+//Создание классов и методов
+//Переменные, их типы (пока только примтивы)
+//Арифметические операции + инкремент\декремент
+//Преобразование типов
+//Вывод в консоль
+//Комментарий (cntrl + слеш  айдеешке)
+*/
