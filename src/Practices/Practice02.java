0340c4d2d8ddddb1895a5dd692d50bc8554d70f4
Alexey <vaznoe2024@mail.ru>
2025-10-09 21:07:59 +0300

    v.4 Добавлена практика Калькулятор

---
+++ src/Practices/Practice02.java
@@ -0,0 +1,46 @@
+// Каждый раз использовать новый сканер - плохая идея, лучше сделать один сканер в мейн
+
+package Practices;
+
+import java.util.Scanner;
+
+public class Practice02 {
+    public static void main(String[] args){
+        Calc();
+    }
+
+    public static void Calc(){
+        while (true) {
+            System.out.print("Введите первое число: ");
+            int a = getNumber();
+            System.out.print("Введите второе число: ");
+            int b = getNumber();
+            System.out.print("Выберете действие (+, -, *, /): ");
+            String op = getOperation();
+            calculate(op, a, b);
+            System.out.println();
+        }
+    }
+
+    public static int getNumber(){
+        Scanner scan = new Scanner(System.in);
+        return scan.nextInt();
+    }
+
+    public static String getOperation(){
+        Scanner scan = new Scanner(System.in);
+        return scan.nextLine();
+    }
+
+    public static void calculate(String t, int a, int b){
+        if (t.equals("+")){
+            System.out.print("Сумма чисел = " + (a + b));
+        } else if (t.equals("-")){
+            System.out.print("Сумма чисел = " + (a - b));
+        } else if (t.equals("*")){
+            System.out.print("Сумма чисел = " + (a * b));
+        } else if (t.equals("/")){
+            System.out.print("Сумма чисел = " + (a / b));
+        }
+    }
+}
