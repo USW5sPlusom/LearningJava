386fce382ec9f22318cbbb271718e273aec7a89e
Alexey <vaznoe2024@mail.ru>
2025-10-12 17:24:31 +0300

    v.8 Добавлена папка для отработки жадных алгоритмов, обновлен файл README

---
+++ src/Algorithms/TwoPointers/TwoPointers03.java
@@ -0,0 +1,13 @@
+package Algorithms.TwoPointers;
+
+//Нахождение длины максимального подмассива дающего таргет
+public class TwoPointers03 {
+    public static void main(String[] args){
+
+    }
+
+    public static int twoPointers(int t, int[] arr){
+
+        return 0;
+    }
+}
